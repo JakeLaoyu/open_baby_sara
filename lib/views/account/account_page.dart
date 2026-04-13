e3f205789b1b421136d8deef3963da4dee416f93
suleymansurucu <suleymansurucu95@gmail.com>
2026-04-13 08:49:29 -0400

    Feat: ✨Remove debug notification test panel from account page

--- lib/views/account/account_page.dart
+++ lib/views/account/account_page.dart
@@ -1,6 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
-import 'package:open_baby_sara/views/debug/notification_test_panel.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:in_app_review/in_app_review.dart';
 import 'package:open_baby_sara/app/routes/app_router.dart';
@@ -420,9 +419,6 @@ class _AccountPageState extends State<AccountPage> {
                             ),
                           ),
                         ),
-
-                        /// Debug notification test panel (kDebugMode only)
-                        const NotificationTestPanel(),
 
                         /// Footer
                         Padding(
